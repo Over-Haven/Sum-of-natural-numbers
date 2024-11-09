f61ba8cb6f4185450166fbbcad36d65c27d23103
Over-haven <tanushdas26@gmail.com>
2024-11-09 20:28:56 +0530

    hki

---
+++ main.py
@@ -0,0 +1,10 @@
+#input
+n = int(input("Enter the value of term: "))
+
+sum = 0#initialise
+i = 1#initialise
+while i <=n: #loop will run from 1 to n
+    sum = sum+i
+    i = i+1
+
+print("\n sum =",sum)
